88881f546dbbbde7dbe7ea04b1af8a7071e9ce71
iced-code github <ayaanmodak1@gmail.com>
2025-06-24 15:37:26 -0400

    User auth (WIP)

--- src/App.js
+++ src/App.js
@@ -9,55 +9,80 @@ import {
   serverTimestamp,
 } from "firebase/firestore";
 
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+function signIn() {
+  signInWithPopup(auth, provider).then((result) => {
+    console.log("Signed in as: ", result.user.uid);
+  }).catch((error) => {
+    console.error("login error:", error);
+  });
+}
+
 function App() {
   const [emoji, setEmoji] = useState("");
   const [text, setText] = useState("");
   const [statuses, setStatuses] = useState([]);
+  const[user, setUser] = useState(null);
 
   useEffect(() => {
+    
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      setUser(firebaseUser);
+    })
+
     const q = query(collection(db, "statuses"), orderBy("timestamp", "desc"));
-    const unsubscribe = onSnapshot(q, (snapshot) => {
+    const unsubscribe2 = onSnapshot(q, (snapshot) => {
       setStatuses(snapshot.docs.map((doc) => doc.data()));
     });
-    return () => unsubscribe();
+    return () => {unsubscribe(); unsubscribe2();};
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!emoji || !text) return;
+
     await addDoc(collection(db, "statuses"), {
       emoji,
       text,
       timestamp: serverTimestamp(),
+      userID: auth.currentUser.uid,
+      userName: auth.currentUser.displayName
     });
     setEmoji("");
     setText("");
   };
 
   return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+    <div className="App">
       <h1>Status Wall</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={emoji}
-          onChange={(e) => setEmoji(e.target.value)}
-          placeholder="Emoji"
-          maxLength={2}
-        />
-        <input
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="What’s up?"
-          maxLength={50}
-        />
-        <button type="submit">Post</button>
-      </form>
+      
+      {!user && <button onClick={signIn}>Sign in</button>}
+      {user ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            value={emoji}
+            onChange={(e) => setEmoji(e.target.value)}
+            placeholder="Emoji"
+            maxLength={2}
+          />
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="What’s up?"
+            maxLength={50}
+          />
+          <button type="submit">Post</button>
+        </form>
+      ) : (<p>Please sign in</p>)}
 
       <h2>Live Feed</h2>
       <ul>
         {statuses.map((s, i) => (
           <li key={i}>
-            {s.emoji} {s.text}
+            {s.userName}: {s.emoji} {s.text}
           </li>
         ))}
       </ul>
